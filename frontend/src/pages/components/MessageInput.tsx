7d492382597023b7a7b2e621fc13b82f037518c6
Samuel k <skifle507@gmail.com>
2025-03-31 13:58:19 +0300

    add message input

---
+++ frontend/src/pages/components/MessageInput.tsx
@@ -0,0 +1,108 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {
+  EllipsisVertical,
+  FileText,
+  Mic,
+  Send,
+  SmilePlus,
+  SquareUser,
+} from "lucide-react";
+
+const MessageInput = () => {
+  return (
+    <div className="flex items-center p-2 border rounded-lg">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Button variant="ghost">
+              <SmilePlus size={18} />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Add to library</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <div className="flex items-center space-x-2">
+        <Input className="flex-1" placeholder="Type a message" type="text" />
+      </div>
+
+      <div className="flex space-x-3 ml-3">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button variant="ghost">
+                <Send size={18} />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Add to library</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button variant="ghost">
+                <FileText size={18} />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Add to library</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button variant="ghost">
+                <SquareUser size={18} />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Add to library</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button variant="ghost">
+                <Mic size={18} />
+                {/* <MicOff /> */}
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Add to library</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button variant="ghost">
+                <EllipsisVertical size={18} />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Add to library</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+    </div>
+  );
+};
+
+export default MessageInput;
